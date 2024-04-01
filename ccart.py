3d67916838f1a6af34a0807b72750f4369865cf3
MGBrebu <mgbrebu7@gmail.com>
2024-04-01 13:40:23 +0100

    add example running config, add output to file ex.

--- ccart.py
+++ ccart.py
@@ -2,6 +2,30 @@
 # By Mario Brebu, Daniel Baldwin, and Mataz Al-Mashikhi
 # In collaboration with greater Configuroo web application project
 
-import sys
+from ciscoconfparse2 import CiscoConfParse
 
-# Code something!
+config = CiscoConfParse('./configs/Router0_running-config.txt')
+
+out = open("./output/hostname.txt", "w")
+
+# Findings and printing hostname
+for hostname in config.find_parent_objects(['hostname']):
+    host = " ".join(hostname.split()[1:])
+    print(f"Hostname: {host}")
+    out.write(f"Hostname: {host}\n")
+
+out.write("\n")
+
+# Finding interfaces
+for intfaces in config.find_parent_objects(['interface']):
+    intf = " ".join(intfaces.split()[1:])
+    print(f"Interfaces: {intf}")
+    out.write(f"Interfaces: {intf}\n")
+
+out.write("\n")
+
+# Finding shutdown interfaces
+for intfacessh in config.find_parent_objects(['interface', 'shutdown']):
+    intfsh = " ".join(intfacessh.split()[1:])
+    print(f"Shutdown Interfaces: {intfsh}")
+    out.write(f"Shutdown Interfaces: {intfsh}\n")
