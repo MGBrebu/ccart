723aae0fe52ec253bad77f87dd0673f6542368ce
MGBrebu <mgbrebu7@gmail.com>
2024-04-03 15:40:35 +0100

    Put code into functions Report generation - header, footer, datetime, user

--- ccart.py
+++ ccart.py
@@ -1,31 +1,73 @@
+from datetime import datetime
+from ciscoconfparse2 import CiscoConfParse
+
+# ------------------------------------------------
 # Cisco Confuguration Auditing and Reporting Tool
 # By Mario Brebu, Daniel Baldwin, and Mataz Al-Mashikhi
 # In collaboration with greater Configuroo web application project
+# ------------------------------------------------
 
-from ciscoconfparse2 import CiscoConfParse
+# Constants
+# Connection points from webapp
+CFG_FILE = './configs/Router0_running-config.txt'
+CURR_USER = 'configuroo'
+# Init CiscoConfParse with current config path
+config = CiscoConfParse(CFG_FILE)
+# Init file to output to
+out = open("./output/output.txt", "w")
+# Datetime Init
+dt = datetime.now()
+ts = dt.strftime("%Y-%m-%d %H:%M:%S")
 
-config = CiscoConfParse('./configs/Router0_running-config.txt')
+# === REPORT GENERATION ===
+def generateReportHeader():
+    out.write("=== REPORT ===\n")
+    out.write("Date/Time Generated: " + ts + "\n")
+    out.write("Current User: " + CURR_USER + "\n")
+    out.write("Configuration File: " + CFG_FILE + "\n\n")
+    out.write("=== FINDINGS ===\n")
 
-out = open("./output/hostname.txt", "w")
+def generateReportFooter():
+    out.write("=== END OF REPORT ===\n")
+    out.write("\nSecurity Score: NaN\n")
+    out.close()
+# =========================
 
-# Findings and printing hostname
-for hostname in config.find_parent_objects(['hostname']):
-    host = " ".join(hostname.split()[1:])
-    print(f"Hostname: {host}")
-    out.write(f"Hostname: {host}\n")
-
-out.write("\n")
+# === SECURITY CHECKS ===
+# Findings hostname
+def findHostname():
+    for hostname in config.find_parent_objects(['hostname']):
+        host = " ".join(hostname.split()[1:])
+        print(f"Hostname: {host}")
+        out.write(f"Hostname: {host}\n")
 
 # Finding interfaces
-for intfaces in config.find_parent_objects(['interface']):
-    intf = " ".join(intfaces.split()[1:])
-    print(f"Interfaces: {intf}")
-    out.write(f"Interfaces: {intf}\n")
-
-out.write("\n")
+def findInterfaces():
+    for intfaces in config.find_parent_objects(['interface']):
+        intf = " ".join(intfaces.split()[1:])
+        print(f"Interfaces: {intf}")
+        out.write(f"Interfaces: {intf}\n")
 
 # Finding shutdown interfaces
-for intfacessh in config.find_parent_objects(['interface', 'shutdown']):
-    intfsh = " ".join(intfacessh.split()[1:])
-    print(f"Shutdown Interfaces: {intfsh}")
-    out.write(f"Shutdown Interfaces: {intfsh}\n")
+def findShutdownInterfaces(): 
+    for intface_sh in config.find_parent_objects(['interface', 'shutdown']):
+        intf_sh = " ".join(intface_sh.split()[1:])
+        print(f"Shutdown Interfaces: {intf_sh}")
+        out.write(f"Shutdown Interfaces: {intf_sh}\n")
+
+# Finding if there is a password set
+def findPassword():
+    for password in config.find_objects(r"password"):
+        print(f"Password: {password.text}")
+        out.write(f"Password: {password.text}\n")
+# =========================
+
+# === MAIN ===
+generateReportHeader()
+
+findHostname()
+findInterfaces()
+findShutdownInterfaces()
+findPassword()
+
+generateReportFooter()
