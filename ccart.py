ef7a312a28ccddf7549891ba6e43890dd0630cd5
MGBrebu <mgbrebu7@gmail.com>
2024-04-05 14:08:53 +0100

    hostname & version checking to report header simplified hostname & version checking

--- ccart.py
+++ ccart.py
@@ -23,55 +23,66 @@ out = open("./output/" + reportName, "w")
 # === REPORT GENERATION ===
 # Top of report includes datetime, current user*, and current config file
 # * Placeholder for future implementation
-def generateReportHeader():
-    out.write("=== REPORT ===\n")
+def generateReportHeader(hostname, version):
+    out.write("=== BEGIN REPORT ===\n")
     out.write("Date/Time Generated: " + ts + "\n")
     out.write("Current User: " + CURR_USER + "\n")
     out.write("Configuration File: " + CFG_FILE + "\n\n")
-    out.write("=== FINDINGS ===\n")
+    out.write("# DEVICE INFO\n")
+    out.write("Hostname: " + hostname + "\n")
+    out.write("Version: " + version + "\n\n")
+    out.write("# FINDINGS\n")
 
 # Bottom of report includes security score
 def generateReportFooter():
-    out.write("=== END OF REPORT ===\n")
+    out.write("\n# SCORE")
     out.write("\nSecurity Score: NaN\n")
+    out.write("\n=== END REPORT ===")
     out.close()
 # =========================
 
-# === SECURITY CHECKS ===
-# Findings hostname
-def findHostname():
-    for hostname in config.find_parent_objects(['hostname']):
-        host = " ".join(hostname.split()[1:])
-        print(f"Hostname: {host}")
-        out.write(f"Hostname: {host}\n")
+# === FINDING DEVICE INFO ===
+# Finding version and hostname for report header
+def findVersion():
+    version = config.find_objects(['version'])[0]
+    return str(" ".join(version.text.split()[1:]))
 
+def findHostname():
+    hostname = config.find_objects(['hostname'])[0]
+    return str(" ".join(hostname.text.split()[1:]))
+# =========================
+
+# === SECURITY CHECKS ===
 # Finding interfaces
 def findInterfaces():
     for intfaces in config.find_parent_objects(['interface']):
         intf = " ".join(intfaces.split()[1:])
-        print(f"Interfaces: {intf}")
         out.write(f"Interfaces: {intf}\n")
 
 # Finding shutdown interfaces
 def findShutdownInterfaces(): 
     for intface_sh in config.find_parent_objects(['interface', 'shutdown']):
         intf_sh = " ".join(intface_sh.split()[1:])
-        print(f"Shutdown Interfaces: {intf_sh}")
         out.write(f"Shutdown Interfaces: {intf_sh}\n")
 
 # Finding if there is a password set
 def findPassword():
     for password in config.find_objects(r"password"):
-        print(f"Password: {password.text}")
-        out.write(f"Password: {password.text}\n")
+        if password.text.startswith("password"):
+            out.write(f"Password: {password}\n")
+            print(password)
 # =========================
 
 # === MAIN ===
-generateReportHeader()
+# Generates reporter header with hostname and version
+generateReportHeader(findHostname(), findVersion())
 
+# Generates findings
+findVersion()
 findHostname()
 findInterfaces()
 findShutdownInterfaces()
 findPassword()
 
+# Generates report footer
 generateReportFooter()
